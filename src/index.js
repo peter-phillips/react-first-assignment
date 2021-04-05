f8c1eafaec45e4d6564cb8b1a49cf0032113e3c5
Peter Phillips <petermartinphillips@gmail.com>
2021-04-04 23:16:01 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import MyApp from './MyApp'
+import './index.css'
+
+ReactDOM.render(<MyApp />, document.getElementById('root'));
